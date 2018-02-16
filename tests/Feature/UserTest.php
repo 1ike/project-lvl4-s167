12027bf1156a2e8f330731da22ecd1417ab9347c
1ike <googlohren@gmail.com>
2018-02-16 19:50:18 +0300

    Try fi travis 109.

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -20,7 +20,9 @@ class UserTest extends TestCase
     public function testCreateUser()
     {
         $user = factory(User::class)->make();
-
+        $this->assertDatabaseHas('users', [
+            'email' => $user->email
+        ]);
         $this->post(route('register'), [
                  'name' => $user->name,
                  'email' => $user->email,
